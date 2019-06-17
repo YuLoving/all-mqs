2048176beafab43b2f06cd7f1e14a64c852639de
YuLoving <974456795@qq.com>
2019-06-17 16:42:42 +0800

    更新

--- src/main/java/nj/ccq/yh/provider/config/BeanConfig.java
+++ src/main/java/nj/ccq/yh/provider/config/BeanConfig.java
@@ -1,5 +1,6 @@
 package nj.ccq.yh.provider.config;
 
+import javax.jms.ConnectionFactory;
 import javax.jms.Queue;
 import javax.jms.Topic;
 
@@ -8,6 +9,8 @@ import org.apache.activemq.command.ActiveMQTopic;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.config.JmsListenerContainerFactory;
 
 /**  
 
@@ -38,5 +41,6 @@ public class BeanConfig {
 	public Topic topic() {
 		return new ActiveMQTopic("zh-topic");
 	}
+
 	
 }
